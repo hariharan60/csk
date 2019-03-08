a4a70b76026b8e3472a14f3f90d46f2d806e1b35
hariharan60 <48342588+hariharan60@users.noreply.github.com>
2019-03-08 10:00:59 +0530

    Add files via upload

---
+++ 2.php
@@ -0,0 +1,40 @@
+<?php	
+	$currentdir=getcwd(();
+	$uploadDirectory="/Newfolder/";
+	$errors=[];
+	$fileExtensions=['jpeg','jpg','png'];
+	$filename=$_FILES['myfile']['name'];
+	$filesize=$_FILES['myfile']['size'];
+	$fileTMPname=$_FILES['myfile']['tmp_name'];
+	$imageFileType=strtolower(pathinfo($fileNmae,FATHINFO_EXTENSTION));
+	$uploadPath=(curreantDir.$uploadDirectory.basename($filename);
+	if(isset($_POST['submit']))
+	{
+		if(!in_array($imageFileType,$fileExtensions))
+		{
+			$error[]="This fileextension is not allowed.please upload a JPEG OR PNG file";
+		}
+		if($filesize>2000000)
+		{
+			$errors[]="this file is more than 2mb.sorry,it has to be less than or equal";
+		}
+		if(empty($errors)) {
+			$didupload=move_uploaded_file($fileTMPName,$uploadPath);
+			if($didupload)
+			{
+				echo"The file".basename($fileName)."has been uploaded";
+			}
+			else
+			{
+				echo "an arror occured somewhere.Tryagain or contact the admin";
+			}
+		}
+		else{
+			foreach($errors as $error)
+			{
+				echo $error."There are the errors"."\n";
+				
+			}
+		}
+		
+	}
